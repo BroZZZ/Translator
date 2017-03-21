c15071a9fb503f505680ccd07eec4afd6d6efc2b
BroZZZ <bukawr@gmail.com>
2017-03-21 13:34:08 +0300

    added few lib in gradle build

--- app/src/main/java/com/translator/brozzz/translator/MainActivity.java
+++ app/src/main/java/com/translator/brozzz/translator/MainActivity.java
@@ -1,7 +1,7 @@
 package com.translator.brozzz.translator;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
